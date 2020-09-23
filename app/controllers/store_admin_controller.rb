bd1737404a711a3a905e751667673088f8a8b2e0
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-09-23 22:36:27 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -6,6 +6,11 @@ class StoreAdminController < ApplicationController
   end 
   
   def orders 
-    render :layout => "order_administr"
+    render :layout => "order_administration"
+  end 
+  
+  def invoice
+    render :layout => false 
+  end 
 
 end
